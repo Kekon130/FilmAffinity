ad5b153ea39b2220395aa04daaabc1d7b642548e
Kekon130 <72703746+Kekon130@users.noreply.github.com>
2025-11-22 17:00:16 +0100

    Spring Boot proyect initialized

---
+++ src/main/java/es/uah/filmAffinity/FilmAffinityApplication.java
@@ -0,0 +1,13 @@
+package es.uah.filmAffinity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmAffinityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmAffinityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/uah/filmAffinity/FilmAffinityApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uah.filmAffinity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmAffinityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
